63ea90d9b982d612c910750a7da09495eb69ba9b
“Magali <maguinavarro07@gmail.com>
2024-08-13 22:11:28 -0300

    Uso de rutas y config en app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "font-awesome/css/font-awesome.css";
@@ -9,5 +9,5 @@ import "font-awesome/css/font-awesome.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
